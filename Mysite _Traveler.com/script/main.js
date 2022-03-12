4d7bd1222133eada209909e51a41677d06c4f68d
KoichiSuzuki <99689018+KoichiSuzuki@users.noreply.github.com>
2022-03-12 23:15:02 +0900

    Add files via upload

---
+++ Mysite _Traveler.com/script/main.js
@@ -0,0 +1,17 @@
+// $(function(){
+//   $('.menu_btn').on('click', function(){
+//     menu_btn();
+//   });
+//   $('.menu-item a').on('click', function(){
+//     menu_btn();
+//   });
+// });
+
+// function menu_btn(){
+//   $('.menu_btn').toggleClass('open');
+//   if($('menu_btn').hasClass('open')){
+//     $('menu-item').addClass('open');
+//   }else{
+//     $('menu-item').removeClass('open');
+//   }
+// }
